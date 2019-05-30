f90c2583cefaa75deb6c69cf62b90bbca402f215
Volodymyr <Pohoriliak>
2019-05-30 16:39:27 +0300

    test

--- src/modules/action-panel/action-panel.module.js
+++ src/modules/action-panel/action-panel.module.js
@@ -6,8 +6,12 @@ import buttonStyles from '../../components/button/button.module.scss';
 
 const baseClassName = 'action-panel';
 
-export default ({ path }) => (
-  <div className={styles[baseClassName]}>
-    <Link to={path} className={buttonStyles.button}>Продовжити</Link>
-  </div>
-);
+export default ({ path }) => {
+  console.log('process.env.PUBLIC_URL', process.env.PUBLIC_URL)
+  console.log('process.env', process.env)
+  return (
+    <div className={styles[baseClassName]}>
+      <Link to={path} className={buttonStyles.button}>Продовжити</Link>
+    </div>
+  );
+}
